e654f4dac2a93c9f101f9fc06adb887f8a74a46f
Muhammad Abbas <anwarabbas22.22@gmail.com>
2021-10-08 12:12:09 +0500

    added

---
+++ Js/app.js
@@ -0,0 +1,64 @@
+let numberfact = document.getElementById("numberfact");
+let numberinput = document.getElementById("numberinput");
+let numbercontainer = document.getElementById("numbercontainer");
+numberinput.addEventListener("input", () => {
+  let numberfacttext = numberinput.value;
+
+  // With Ajax
+  // let request =new XMLHttpRequest();
+  // request.open('GET' , `http://numbersapi.com/${numberfacttext}`)
+  // request.onload = function(){
+  //   if (this.status = '200' && numberfacttext != '') {
+  //     let response = this.responseText;
+  //     numbercontainer.style.display = 'block';
+  //     numberfact.innerHTML = response;
+  //   } else {
+  //     numbercontainer.style.display = 'none';
+  //   }
+  // }
+  // ()request.send
+
+  // With Fetch api***
+  fetch(`http://numbersapi.com/${numberfacttext}`)
+    .then((response) => response.text())
+    .then((data) => {
+      if (numberfacttext != "") {
+        numbercontainer.style.display = "block";
+        numberfact.innerHTML = data;
+      } else {
+        console.error("Enter a number");
+      }
+    });
+});
+
+// Year fact
+let yearfact = document.getElementById("yearfact");
+let yearinput = document.getElementById("yearinput");
+let yearcontainer = document.getElementById("yearcontainer");
+yearinput.addEventListener("input", () => {
+  let yearfacttext = yearinput.value;
+  //   let request = new XMLHttpRequest();
+  //   request.open("GET", `http://numbersapi.com/${yearfacttext}/year`);
+  //   request.onload = function () {
+  //       if (this.status = '200' && yearfacttext != '') {
+  //           let data = this.responseText;
+  //           yearcontainer.style.display = 'block';
+  //           yearfact.innerText = data;
+  //         } else {
+  //           yearcontainer.style.display = 'none';
+  //       }
+  //   };
+  //   request.send();
+
+  //   Fetch api
+  fetch(`http://numbersapi.com/${yearfacttext}/year`)
+    .then((response) => response.text())
+    .then((data) => {
+     if (yearfacttext != '') {
+        yearcontainer.style.display = "block";
+        yearfact.innerText = data;
+    } else {
+        yearcontainer.style.display = "none";
+     }
+    });
+});
